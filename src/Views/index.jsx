589d4571eb48bdc909d2f73ac9a573a34e472c1e
arifinlenna <arifin@lenna.ai>
2023-07-21 07:52:19 +0700

    update padding

--- src/Views/index.jsx
+++ src/Views/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function index() {
   return (
-    <div>
+    <div className='px-4 md:px-0'>
         <h4>Hallo, pengunjung</h4>
         <p>Selamat datang di portal berita, silahkan anda pilih berita di navbar</p>
     </div>
